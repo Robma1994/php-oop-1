6bdd3aaf38184b17de223971783d78c88cd8e197
Roberto <robertomartinonwg@gmail.com>
2021-10-21 15:23:01 +0200

    Esercizio completato

--- index.php
+++ index.php
@@ -1,29 +1,49 @@
 <?php
 
-    //è definita una classe ‘Movie’
-   /*all'interno della classe: 
-    1) Sono dichiarate delle variabili d'istanza
-    2) è definito un costruttore
-    3) è definito almeno un metodo
-    4) Vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-   */
+
+// DEFINIRE UNA CLASSE
     class Movie {
+// DEFINIRE DELLE VARIABILI
         public $title;
         public $subTitle;
         public $year;
-        
+        public $genre;
+    
+// DEFINIRE IL COSTRUTTORE
         function __construct($_title, $_subTitle, $_year) {
             $this->title = $_title;
             $this->subTitle = $_subTitle;
             $this->year = $_year;
         }
+
+// DEFINIRE I METODI
+        function addGenre($_genre) {
+            return $this->genre = $_genre;
+        }
     }
 
+// 4) Vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
     $harryPotter = new Movie('Harry Potter', 'Il calice di fuoco', 2005);
     echo 'Title :' . ' ' . $harryPotter->title . '<br/>' ;
     echo 'SubTitle :' . ' ' . $harryPotter->subTitle . '<br/>' ;
     echo 'Anno :' . ' ' . $harryPotter->year . '<br/>' ;
-    
+    echo 'Genre :' . ' ' . $harryPotter->addGenre('Narrativa fantasy') . '<br/>';
+    echo '<br/>';
+
+
+    $rocky = new Movie('Rocky1', 'x', 2001);
+    echo 'Title :' . ' ' . $rocky->title . '<br/>' ;
+    echo 'SubTitle :' . ' ' . $rocky->subTitle . '<br/>' ;
+    echo 'Anno :' . ' ' . $rocky->year . '<br/>' ;
+    echo 'Genre :' . ' ' . $harryPotter->addGenre('Narrativa fantasy') . '<br/>';
+    echo '<br/>';
+
+    $ilSignoreDegliAnelli = new Movie('Signore Degli Anelli', 'x', 1990);
+    echo 'Title :' . ' ' . $ilSignoreDegliAnelli->title . '<br/>' ;
+    echo 'SubTitle :' . ' ' . $ilSignoreDegliAnelli->subTitle . '<br/>' ;
+    echo 'Anno :' . ' ' . $ilSignoreDegliAnelli->year . '<br/>' ;
+    echo 'Genre :' . ' ' . $harryPotter->addGenre('Narrativa fantasy') . '<br/>';
+ 
 ?>
 
 
